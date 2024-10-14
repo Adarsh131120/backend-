4c7e233100118d36935c7ea629edf3124c717455
Adarsh Tiwari personal <adarshtiwari131120@gmail.com>
2024-10-14 07:10:32 +0530

    connecting database

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import mongoose, { connect } from "mongoose";
+
+
+
+// require('dotenv').config({path : './env'})
+
+import connectDB from "./db/index.js";
+
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './env'
+})
+
+
+
+// function connectDB(){}
+
+
+connectDB();  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+
+// const app = express();
+
+
+// (async()=>{
+//     try{
+//  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//    app.on("error",()=>{
+//     console.log("Error connecting to database")
+//     throw error;
+//    })
+
+//    app.listen(process.env.PORT,()=>{
+//     console.log(`Server is running on port ${process.env.PORT}`)
+//     }
+// )}
+
+//     catch(error){
+//         console.log("ERROR :" ,error)
+//         throw error;
+//     }
+// })()
+
+// connectDB();
